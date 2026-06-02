8683eafb299189470a991ce375dc4f605dfb34e2
Devaji Patil <devaji.patil@arena2036.de>
2026-06-02 11:55:24 +0200

    chore:copyright header update

--- ihub-frontend/src/features/credentials/api.ts
+++ ihub-frontend/src/features/credentials/api.ts
@@ -1,5 +1,4 @@
 /********************************************************************************
- * Copyright (c) 2026 ARENA2036 e.V.
  * Copyright (c) 2026 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
